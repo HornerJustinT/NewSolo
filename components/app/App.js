66bdc55922baac006a4f77820dbe5505241f16b2
HornerJustinT <Horner.Justin.T@gmail.com>
2020-04-30 13:24:24 -0500

    crypto install

--- components/app/App.js
+++ components/app/App.js
@@ -48,11 +48,15 @@ import Auth0 from "react-native-auth0";
 import Home from "../Home/Home"
 var credentials = require("../Login/auth0-configuration"); // has info from my
 const auth0 = new Auth0(credentials);
+var request = require("request");
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { accessToken: null, username: null };
   }
+  
   _onLogin = () => {
     auth0.webAuth
       .authorize({
@@ -61,6 +65,17 @@ class App extends Component {
       .then(credentials => {
         Alert.alert("AccessToken: " + credentials.accessToken);
         this.setState({ accessToken: credentials.accessToken });
+        var options = {
+          method: 'GET',
+          url: 'https://dev-e8l8uqk7.auth0.com/api/v2/users',
+          qs: {q: 'email:"jane@exampleco.com"', search_engine: 'v3'},
+          headers: {authorization: `Bearer ${credentials.accessToken}`}
+        };
+        request(options, function (error, response, body) {
+          if (error) throw new Error(error);
+        
+          console.log(body);
+        });
         // Parse the URL and extract the Access Token
         auth0.auth
           .userInfo({ token: credentials.accessToken })
---
+++ server/server.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+// Route includes
+const userRouter = require('./routes/user.router');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/* Routes */
+app.use('/api/user', userRouter);
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
